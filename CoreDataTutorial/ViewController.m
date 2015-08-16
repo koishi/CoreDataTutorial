48125a475064830a56198f62afe6d6bbd28ef3d2
Koichiro Oishi <bs@v7.com>
2015-08-16 11:44:52 +0900

    self.locationManager初期化ロジックの仮実装

--- CoreDataTutorial/ViewController.m
+++ CoreDataTutorial/ViewController.m
@@ -16,24 +16,58 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+
+  // タイトルを設定する。 self.title = @"Locations";
+  // ボタンをセットアップする。 self.navigationItem.leftBarButtonItem = self.editButtonItem;
+  self.addButton = [[UIBarButtonItem alloc]
+               initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+               target:self action:@selector(addEvent)];
+  self.addButton.enabled = NO;
+  self.navigationItem.rightBarButtonItem = self.addButton;
+
+  // locationManagerの処理
+  self.locationManager = [CLLocationManager new];
+  [self.locationManager setDelegate:self];
+  [self.locationManager setDistanceFilter:kCLDistanceFilterNone];
+  [self.locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+  
+  if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+    [self.locationManager requestWhenInUseAuthorization];
+  }
+  
+  [[self locationManager] startUpdatingLocation];
+}
+
+- (void)viewDidUnload {
+  self.eventsArray = nil;
+  self.locationManager = nil;
+  self.addButton = nil;
 }
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
 }
 
 #pragma mark -
 
-- (CLLocationManager *)locationManager {
-  if (self.locationManager != nil) {
-    return self.locationManager;
-  }
-  self.locationManager = [[CLLocationManager alloc] init];
-  self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
-  self.locationManager.delegate = self;
-  return self.locationManager;
+//- (CLLocationManager *)locationManager {
+//  if (self.locationManager != nil) {
+//    return self.locationManager;
+//  }
+//  self.locationManager = [[CLLocationManager alloc] init];
+//  self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+//  self.locationManager.delegate = self;
+//  return self.locationManager;
+//}
+
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation {
+  self.addButton.enabled = YES;
+}
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error {
+  self.addButton.enabled = NO;
 }
 
 @end
